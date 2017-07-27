e21f128761720b84e6ceb9cf2f9e66b8199e9099
brwingo <brwingo@ncsu.edu>
2017-07-27 14:45:08 -0400

    fixing master

---
+++ ScrumbledorsArmy/src/main/java/com/revature/pojo/Role.java
@@ -0,0 +1,55 @@
+package com.revature.pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "ROLE")
+public class Role implements Serializable {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -3443225460812901410L;
+
+  @Id
+  @Column(name = "R_ID")
+  private int id;
+  @Column(name = "R_ROLE")
+  private String role;
+
+  public Role() {
+  }
+
+  public Role(int id, String role) {
+    super();
+    this.id = id;
+    this.role = role;
+  }
+
+  @Override
+  public String toString() {
+    return "Role [id=" + id + ", role=" + role + "]";
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+}
