4e430c82ca00f53a2abf5da0f1b7cd9b04404d66
roktimb <barkakatirb@gmail.com>
2017-07-29 16:09:25 -0400

    Adding LogDaoImpli

---
+++ ScrumbledorsArmy/src/main/java/com/revature/dao/LogDaoImpli.java
@@ -0,0 +1,79 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.revature.pojo.Log;
+import com.revature.pojo.Story;
+@Repository
+@Component
+public class LogDaoImpli implements Dao<Log> {
+	 @Autowired
+	 private SessionFactory sessionFactory;
+
+
+	@Override
+	public void insert(Log obj) {
+		 System.out.println("inserting a Log");
+	        Session session = sessionFactory.getCurrentSession();
+	        session.save(obj);
+		
+	}
+
+	@Override
+	public Log getPojoById(Log obj) {
+		System.out.println("Pulling a Story by ID");
+        Session session = sessionFactory.getCurrentSession();
+		return (Log)session.get(Log.class,obj.getId());
+		
+	}
+
+	@Override
+	public List<Log> getAllPojos() {
+		System.out.println("Getting all Logs");
+        Session session= sessionFactory.getCurrentSession();
+        Criteria crit=session.createCriteria(Log.class);// TODO Auto-generated method stub
+		List<Log> logs=crit.list();
+        return logs;
+	
+	}
+
+	@Override
+	public List<Log> getAllPojos(List<Criterion> restrictions) {
+		System.out.println("Getting all Logs with Criteria");
+        Session session= sessionFactory.getCurrentSession();
+        Criteria crit=session.createCriteria(Log.class);
+        for(Criterion a:restrictions){
+            
+            crit.add(a);
+            
+        }
+        List<Log> logs=crit.list();
+		return logs;
+		
+	}
+
+	@Override
+	public void update(Log obj) {
+		System.out.println("Updating a Log");
+        Session session = sessionFactory.getCurrentSession();
+        session.update(obj);
+		
+	}
+
+	@Override
+	public void delete(Log obj) {
+		System.out.println("Deleting a Log");
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(obj);
+		
+	}
+
+}
