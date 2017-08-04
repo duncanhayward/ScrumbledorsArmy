89c207ccdae2a4c3c056f9f810c544243a998658
JaydeepBhatia <jaydeep.bhatiamain@gmail.com>
2017-08-03 22:38:13 -0400

    Register Works

--- ScrumbledorsArmy/src/main/java/com/revature/controller/RegisterCtrl.java
+++ ScrumbledorsArmy/src/main/java/com/revature/controller/RegisterCtrl.java
@@ -8,13 +8,34 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.pojo.User;
+import com.revature.service.AppService;
 import com.revature.service.AppServiceImpl;
 
-
+@RestController
 public class RegisterCtrl {
 
+	@Autowired
+	AppService app;
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<User> loginUser(@RequestBody User user) {
+		System.out.println("registering exsisting user");
+		app.insertUser(user);
+//		if (userReturned != null) {
+//			return new ResponseEntity<User>(userReturned, HttpStatus.OK);
+//		}
+//		else {
+//			return new ResponseEntity<User>(HttpStatus.IM_USED);
+//		}
+
+		System.out.println("User="+user);
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+	
+	
 //    
 //    @Autowired
 //    AppServiceImpl app;
