ae110ac80f262323c0be8a9a4a92f055b33ead4b
roktimb <barkakatirb@gmail.com>
2017-08-05 17:47:00 -0400

    adding Chart toggle checkbox

--- ScrumbledorsArmy/src/main/webapp/static/script.js
+++ ScrumbledorsArmy/src/main/webapp/static/script.js
@@ -1,6 +1,25 @@
 /**
  * 
  */
+Date.prototype.addDays = function(days) {
+    var dat = new Date(this.valueOf())
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
+
+function getDates(startDate, stopDate) {
+    var dateArray = new Array();
+    var currentDate = startDate;
+    while (currentDate <= stopDate) {
+        dateArray.push( new Date (currentDate) )
+        currentDate = currentDate.addDays(1);
+    }
+    return dateArray;
+}
+var range=getDates(new Date(2012, 08, 1),new Date(2012, 08, 15));
+console.log(range);
+var y=[101,99,95,80,75,68,50,45,42,39,35,20,15,0];
+var dps=[];
 console.log('script was called')
 
 var app = angular
@@ -130,14 +149,60 @@ var app = angular
 		})
 					
 					
-		.controller("BurndownCtrl", function($scope, $http, $location) {
-			console.log('burndown Controller callled');
-			$scope.changeToBurndown = function() {
+.controller('ChartController', function($scope) {
+	 $scope.chart = new CanvasJS.Chart("chartContainer", {
+		 
+		 
+		 	theme: 'theme1',
+		 
+		    title:{
+		    	text: "Basic Burndown chart" 
+		    	},
+		    	
+		    animationEnabled: true,
+		    axisY: {
+		             includeZero: true,
+		         },
+		 
+		    axisX: {
+		        	 valueFormatString:'DD',
+		        	 
+		    
+		        	 interval: 1,
+		        	 
+		        	 intervalType:"day"
+		         },
+		    toolTip:{
+		    	enabled:false
+		    	
+		    },
+		         
+		         data: [{
+		        	 		type: "area",
+		        	        markerSize:8,
+		        	        dataPoints:dps
+		        	 
+		        	       }]
+		        	 
+		        	     });
+	 function parseDatapoints(){
+		 for(var i=dps.length; i<range.length;i++){
+			 dps.push({
+				 x:range[i],
+				 y:y[i]
+			 });	
+		 }
+	 };
+	 function addDatapoints(){
+		 parseDatapoints();
+		$scope.chart.options.data[0].dataPoints=dps;
+		 $scope.chart.render();
+	 }
+	 addDatapoints();
+	 
 
-				$location.path('/burndown');
-			}
 
-		})
+})
 			
 
 		.controller("scrumPageCtrl", function($scope, $http, $location) {
