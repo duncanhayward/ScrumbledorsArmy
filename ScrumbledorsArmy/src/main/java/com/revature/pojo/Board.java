d90e06007f01769a8771262e4a1da6b9de950472
brwingo <brwingo@ncsu.edu>
2017-08-07 00:24:57 -0400

    still merging for some reason?

--- ScrumbledorsArmy/src/main/java/com/revature/pojo/Board.java
+++ ScrumbledorsArmy/src/main/java/com/revature/pojo/Board.java
@@ -10,9 +10,11 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
