864f6e0b7a4a48395821473e5a1676d4b3667233
Troy_K <textak64@gmail.com>
2017-08-06 22:37:05 -0400

    done except getAll for boardrole due to multiplebagfetchexception

--- ScrumbledorsArmy/src/test/java/com/revature/DaoImplTests.java
+++ ScrumbledorsArmy/src/test/java/com/revature/DaoImplTests.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import static org.junit.Assert.*;
+
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,40 +10,141 @@ import java.util.List;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.dao.*;
 
 import com.revature.pojo.*;
 
+@ContextConfiguration(locations = "classpath:beans.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 public class DaoImplTests {
 
-  @Test
-  public void testDao() {
-    Dao<User> userDao = new UserDaoImpl();
-    Dao<Role> roleDao = new RoleDaoImpl();
-    Dao<Board> boardDao = new BoardDaoImpl();
-    Dao<SwimLane> swimLaneDao = new SwimLaneDaoImpl();
-    Dao<Story> storyDao = new StoryDaoImpl();
-    Dao<Task> taskDao = new TaskDaoImpl();
-    Dao<BoardChart> boardChartDao = new BoardChartDaoImpl();
-    Dao<BoardRole> boardRoleDao = new BoardRoleDaoImpl();
+  @Autowired
+  private Dao<User> userDao;
+  @Autowired
+  private Dao<Board> boardDao;
+  @Autowired
+  private Dao<Role> roleDao;
+  @Autowired
+  private Dao<BoardRole> boardRoleDao;
+  @Autowired
+  private Dao<SwimLane> swimLaneDao;
+  @Autowired
+  private Dao<Story> storyDao;
+  @Autowired
+  private Dao<Task> taskDao;
+  @Autowired
+  private Dao<BoardChart> boardChartDao;
 
-    User user = new User(0, "username", "password", "email");
+  @Test
+  @Rollback(true)
+  public void testDao() {
+
+    User user = new User();
+    user.setUsername("username");
+    user.setPassword("password");
+    user.setEmail("email");
     Role role = new Role(0, "role");
-    Board board = new Board(-1, "name", "description");
-    SwimLane swimLane = new SwimLane(0, board, "name", 0);
-    Story story = new Story(0, swimLane, 0, null, new Timestamp(System.currentTimeMillis()),
-        new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), 'N');
-    Task task = new Task(0, story, null, 'N');
-    BoardChart boardChart = new BoardChart(0, board, new Timestamp(System.currentTimeMillis()),
-        new Timestamp(System.currentTimeMillis()), "Test Board Chart");
+    Board board = new Board();
+    board.setName("name");
+    board.setDescription("description");
+    SwimLane swimLane = new SwimLane();
+    swimLane.setBoard(board);
+    swimLane.setName("name");
+    swimLane.setOrder(0);
+    Story story = new Story();
+    story.setSwimLane(swimLane);
+    story.setDescription("description");
+    story.setStart(new Timestamp(System.currentTimeMillis()));
+    story.setEndExpected(new Timestamp(System.currentTimeMillis()));
+    story.setEndActual(new Timestamp(System.currentTimeMillis()));
+    story.setDone('N');
+    Task task = new Task();
+    task.setStory(story);
+    task.setDescription("description");
+    task.setDone('N');
+    BoardChart boardChart = new BoardChart();
+    boardChart.setBoard(board);
+    boardChart.setStart(new Timestamp(System.currentTimeMillis()));
+    boardChart.setEnd(new Timestamp(System.currentTimeMillis()));
+    boardChart.setName("name");
     BoardRole boardRole = new BoardRole(new BoardRoleId(user, board), role);
 
-    boardChartDao.insert(boardChart);
-    boardChart = boardChartDao.getPojoById(boardChart);
+    assertNotNull(user);
+    assertNotNull(role);
+    assertNotNull(board);
+    assertNotNull(swimLane);
+    assertNotNull(story);
+    assertNotNull(task);
+    assertNotNull(boardChart);
+    assertNotNull(boardRole);
 
-    List<Criterion> restrictions = new ArrayList<>(Arrays.asList(Restrictions.ilike("BD_NAME", "")));
-    boardChartDao.getAllPojos(restrictions);
+    user.setId(userDao.insert(user));
+    role.setId(roleDao.insert(role));
+    board.setId(boardDao.insert(board));
+    swimLane.setId(swimLaneDao.insert(swimLane));
+    story.setId(storyDao.insert(story));
+    task.setId(taskDao.insert(task));
+    boardChart.setId(boardChartDao.insert(boardChart));
+    boardRoleDao.insert(boardRole);
+
+    user = userDao.getPojoById(user);
+    role = roleDao.getPojoById(role);
+    board = boardDao.getPojoById(board);
+    swimLane = swimLaneDao.getPojoById(swimLane);
+    story = storyDao.getPojoById(story);
+    task = taskDao.getPojoById(task);
+    boardChart = boardChartDao.getPojoById(boardChart);
+    boardRole = boardRoleDao.getPojoById(boardRole);
+
+    assertNotNull(user);
+    assertNotNull(role);
+    assertNotNull(board);
+    assertNotNull(swimLane);
+    assertNotNull(story);
+    assertNotNull(task);
+    assertNotNull(boardChart);
+    assertNotNull(boardRole);
+
+    /*List<Criterion> restrictions = new ArrayList<>();
+    restrictions.add(Restrictions.ilike("name", "name"));
+    List<BoardChart> allCharts = boardChartDao.getAllPojos(restrictions);
+
+    assertNotNull(allCharts);*/
+
+    userDao.update(user);
+    roleDao.update(role);
+    boardDao.update(board);
+    swimLaneDao.update(swimLane);
+    storyDao.update(story);
+    taskDao.update(task);
+    boardChartDao.update(boardChart);
+    boardRoleDao.update(boardRole);
+
+    assertNotNull(user);
+    assertNotNull(role);
+    assertNotNull(board);
+    assertNotNull(swimLane);
+    assertNotNull(story);
+    assertNotNull(task);
+    assertNotNull(boardChart);
+    assertNotNull(boardRole);
+
+    boardRoleDao.delete(boardRole);
+    boardChartDao.delete(boardChart);
+    taskDao.delete(task);
+    storyDao.delete(story);
+    swimLaneDao.delete(swimLane);
+    boardDao.delete(board);
+    roleDao.delete(role);
+    userDao.delete(user);
 
   }
 }
